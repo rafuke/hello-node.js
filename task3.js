85d8245d7a3477de20d36a662b19c3b39c575b83
unknown <toomaste@U-423-31.edu.wks.metropolia.fi>
2014-09-09 19:38:10 +0300

    Added dateformat npm module

--- task3.js
+++ task3.js
@@ -1,11 +1,17 @@
 var m=require("./last-mod");
+var dateFormat = require('dateformat');
+
 var timestamp=m(process.argv[2]);
+
 if(timestamp !== false){
 	var data=new Date(timestamp);
 	
-     var fin=data.getDate()+"-"+(data.getMonth()+1)+"-"+data.getFullYear()+" ";
-	 fin+=' '+data.getHours()+":"+data.getMinutes(); 
-	 console.log(fin);
+	var formattedDate = dateFormat(data, "dd.mm.yyyy H:MM:ss");
+	
+	var fin=data.getDate()+"-"+(data.getMonth()+1)+"-"+data.getFullYear()+" ";
+	fin+=' '+data.getHours()+":"+data.getMinutes(); 
+	
+	console.log(formattedDate);
 }else{
 	 console.log("Error file name");
 }
