d9e1cab5d62a480d45f68b238fc189d72ec34e1c
raafaelk <rafuke@fe.fi>
2014-09-16 18:49:42 +0300

    Lisätty koodin validointi ja npm

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-hello-node.js
-=============
+ASI sallii joidenkin merkkien puutumisen syntaksissa, 
+esin joskus puolipisteiden. Jos se on päällä ja
+rivinvaihto on tullut tietyyn kohtaan ASI lisää 
+puolipisteen rivin loppuun, aikaisemmin toimiva koodi ei
+enää toimi.
+
+
--- task3.js
+++ task3.js
@@ -1,11 +1,10 @@
-var m=require("./last-mod");
-var timestamp=m(process.argv[2]);
+var m = require("./last-mod");
+var timestamp = m(process.argv[2]);
 if(timestamp !== false){
-	var data=new Date(timestamp);
-	
-     var fin=data.getDate()+"-"+(data.getMonth()+1)+"-"+data.getFullYear()+" ";
-	 fin+=' '+data.getHours()+":"+data.getMinutes(); 
-	 console.log(fin);
+	var data = new Date(timestamp);
+     var fin = data.getDate() + "-" + (data.getMonth() + 1) + "-" + data.getFullYear() + " ";
+     fin += " " + data.getHours() + ":" + data.getMinutes();
+     console.log(fin);
 }else{
-	 console.log("Error file name");
+      console.log("Error file name");
 }
